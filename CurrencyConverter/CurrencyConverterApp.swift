dde57c3cc5bb766d3ae1b746fa800fb9139ea295
Damian Cichosz <damcichosz@gmail.com>
2025-04-19 01:21:42 +0200

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Damian Cichosz on 19/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
